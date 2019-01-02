eea380711f9d4292d62d1009c9e5afa27870a162
Timothee Reynaud <reynaudtimothee@gmail.com>
2019-01-02 19:32:03 +0100

    end of integration + acf begining

--- public_html/wp-content/themes/understrap-child/page-templates/hompepageV2.php
+++ public_html/wp-content/themes/understrap-child/page-templates/hompepageV2.php
@@ -26,21 +26,28 @@ $container = get_theme_mod( 'understrap_container_type' );
 						<!-- <?php the_content();?> -->
 						<!-- <?php the_field('home_page_title');?> -->
                         <div class="landing-page">
+							
 							<div class="service-description">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/logo_mybrian_white.png" alt="" class="logo">
 								<div class="service-description--content">
-									<p class="service-description--subtitle">Le spécialiste de l'anglais</p>
-									<h1 class="service-description--title">La traduction professionnelle simplifiée</h1>
+									
+									<p class="service-description--subtitle"><?php the_field('homepage_surtitre');?></p>
+									<h1 class="service-description--title"><?php the_field('homepage_title');?></h1>
 									<p class="service-description--text">
-										Une traduction 100% humaine, flexible et rapide. 
-										Nos Brians sont des traducteurs natifs et certifiés. 
-										Echangez directement avec votre Brian via chat. 
-										Maîtrisez votre budget et vos délais.
+										<?php the_field('homepage_service_description');?>
 									</p>
-									<a class="cta-main cta" href="#">créer un compte</a>
+									<?php 
+										$link = get_field('homepage_cta_link');
+
+										if( $link ): ?>
+											
+											<a class="cta-main cta" href="<?php echo $link; ?>"><?php the_field('homepage_cta_label');?></a>
+
+										<?php endif; ?>
+									
 								</div>
 								
 								<div class="conversation-bubbles">
-									<img src="../sass/assets/translation-bubble-bg_left.png" alt="">
 									<div class="conversation-bubbles--bubble__first conversation-bubbles--bubble">
 										<span class='conversation-bubbles--text'>J'ai besoin de Brian</span>
 									</div>
@@ -52,45 +59,54 @@ $container = get_theme_mod( 'understrap_container_type' );
 							</div>
 							<div class="quick-quotation">
 								<div class="quick-quotation--tool">
+									<img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/brian-icon-large.png" alt="" class="brian-icon-large">
 								</div>
 								<img src="#" alt="">
 							</div>
 						</div>
 						<div class="why-brian">
 							<h3 class="section-title">
-								Pourquoi Mybrian ?
+								<?php the_field('whybrian_title');?>
 							</h3>
 							<div class="why-brian--items">
 								<div class="why-brian--item">
 									<div class="why-brian--icon">
 										<img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/icon_relationship.png" alt="" class="why-brian--icon__image">
 									</div> 
-									<h4 class="why-brian--title">Choisissez votre délai</h4>
+									<h4 class="why-brian--title"><?php the_field('whybrian_item_1_title');?></h4>
 									<p class="why-brian--text"> 
-										Choisissez votre délai selon votre degré d'urgence.
-										Le prix est calculé automatiquement.
+										<?php the_field('whybrian_item_1_content');?>
 									</p>
 								</div>
 								<div class="why-brian--item">
 									<div class="why-brian--icon">
 										<img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/icon_simplicity.png" alt="" class="why-brian--icon__image">
 									</div> 
-									<h4 class="why-brian--title">Relation directe client-traducteur</h4>
+									<h4 class="why-brian--title"><?php the_field('whybrian_item_2_title');?></h4>
 									<p class="why-brian--text"> 
-										Interaction par chat avec le traducteur depuis votre mobile ou votre ordinateur.
+										<?php the_field('whybrian_item_2_content');?>
 									</p>
 								</div>
 								<div class="why-brian--item">
 									<div class="why-brian--icon">
 										<img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/icon_translation.png" alt="" class="why-brian--icon__image">
 									</div> 
-									<h4 class="why-brian--title">Simplicité d’utilisation</h4>
+									<h4 class="why-brian--title"><?php the_field('whybrian_item_3_title');?></h4>
 									<p class="why-brian--text"> 
-										Postez, chattez, c’est traduit. Paiement par CB ou facturation mensuelle depuis un compte entreprise.
+										<?php the_field('whybrian_item_3_content');?>
 									</p>
 								</div>
 							</div>
-							<a href="#" class="cta">voir la FAQ</a>
+							<?php 
+
+								$link = get_field('whybrian_cta_link');
+
+								if( $link ): ?>
+									
+									<a class="cta" href="<?php echo $link; ?>"><?php the_field('whybrian_cta_label');?></a>
+									
+								<?php endif; ?>
+							
 						</div>
 						<div class="clients">
 							<h3 class="section-title">
@@ -99,7 +115,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 							<ul class="nav nav-tabs" id="myTab" role="tablist">
 								<li class="nav-item">
 									<a class="nav-link active" id="home-tab" data-toggle="tab" href="#testimonial1" role="tab" aria-controls="home" aria-selected="true">
-										<img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/clients_logo/logo_1.png" alt="" class="clients--logo">
+										<?php 
+											$image = get_field('quote_1_image');
+											
+											if( !empty($image) ): ?>
+
+												<img src="<?php echo $image['url']; ?>" class="clients--logo" alt="<?php echo $image['alt']; ?>" />
+											<?php endif; ?>
+										<!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/clients_logo/logo_1.png" alt="" class="clients--logo"> -->
 									</a>
 								</li>
 								<li class="nav-item">
@@ -178,19 +201,19 @@ $container = get_theme_mod( 'understrap_container_type' );
 							</h3>
 							<div class="blog-articles">
 								<div class="blog-article">
-									<img src="https://source.unsplash.com/random" alt="" class='blog-article--image'>
+									<img src="https://source.unsplash.com/random/400x300" alt="" class='blog-article--image'>
 									<h6 class="blog-article--title">Un traducteur en chair et en os</h6>
 									<p class="blog-article--text">Le job de traducteur indépendant ne s’improvise pas. Il y a, par exemple, un monde entre donner une vague idée du sens d’une phrase dans une langue et parler d’un sujet avec style et précision. C’est ce à quoi CSA (Common Sense Advisory) fait allusion en évoquant « Can’t read, won’t buy ».</p>
 									<a href="#" class='next'>lire la suite ...</a>
 								</div>
 								<div class="blog-article">
-									<img src="https://source.unsplash.com/random" alt="">
+									<img src="https://source.unsplash.com/random/400x300" alt="">
 									<h6 class="blog-article--title">Un traducteur en chair et en os</h6>
 									<p class="blog-article--text">Le job de traducteur indépendant ne s’improvise pas. Il y a, par exemple, un monde entre donner une vague idée du sens d’une phrase dans une langue et parler d’un sujet avec style et précision. C’est ce à quoi CSA (Common Sense Advisory) fait allusion en évoquant « Can’t read, won’t buy ».</p>
 									<a href="#" class='next'>lire la suite ...</a>
 								</div>
 								<div class="blog-article">
-									<img src="https://source.unsplash.com/random" alt="">
+									<img src="https://source.unsplash.com/random/400x300" alt="">
 									<h6 class="blog-article--title">Un traducteur en chair et en os</h6>
 									<p class="blog-article--text">Le job de traducteur indépendant ne s’improvise pas. Il y a, par exemple, un monde entre donner une vague idée du sens d’une phrase dans une langue et parler d’un sujet avec style et précision. C’est ce à quoi CSA (Common Sense Advisory) fait allusion en évoquant « Can’t read, won’t buy ».</p>
 									<a href="#" class='next'>lire la suite ...</a>
@@ -227,3 +250,22 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
+
+<script>
+	const nav = document.querySelector(".navbar");
+	
+
+	function displayNav() {
+		console.log(scrollY)
+		if(window.scrollY > 200) {
+			document.body.classList.add("fixed-nav");
+		} else {
+			document.body.classList.remove("fixed-nav");
+		}
+	}
+
+	window.addEventListener("scroll", displayNav);
+
+	const whyBrianHeight = document.querySelector(".why-brian").offsetHeight
+	document.querySelector(".clients").style.marginTop = whyBrianHeight + "px"
+</script>
